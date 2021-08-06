0185af784fe89bbd46ed5f106700a6d0e3dc7865
MementoMori <1003350679@qq.com>
2021-08-06 14:34:12 +0800

    startpart部分测试用例

--- release-assistant/test/test_start/test_start_cli.py
+++ release-assistant/test/test_start/test_start_cli.py
@@ -14,3 +14,180 @@
 """
 TestStart
 """
+import os
+import datetime
+from pathlib import Path
+from test.base.basetest import TestMixin
+from requests.exceptions import RequestException
+from javcra.cli.commands.startpart import StartCommand
+import pandas as pd
+
+EXPECT_DATA_FILE = os.path.join(os.path.abspath(os.path.dirname(__file__)), "mock_data")
+
+
+class TestStart(TestMixin):
+    """
+    class for test TestStart
+    """
+    cmd_class = StartCommand
+
+    def test_success(self):
+        """
+        test success
+        """
+        self.expect_str = """
+[INFO] start update successfully.
+        """
+        resp = self.make_expect_data(200, 'startpart.txt')
+        mock_init_success_data = self.read_file_content('init_success_data.txt', folder=EXPECT_DATA_FILE,
+                                                        is_json=False)
+        mock_init_r = self.make_object_data(200, mock_init_success_data)
+        self.command_params = ["--giteeid=Mary", "--token=release-tools", "I40321"]
+        con = self.read_file_content('mock_obs_data.json', folder=EXPECT_DATA_FILE)
+        for con_key in con['contents']:
+            con_key["key"] = "cve-manager-updateinfo/{}/{}".format(
+                datetime.date(datetime.date.today().year, datetime.date.today().month,
+                              datetime.date.today().day).strftime('%Y-%m-%d'), con_key["key"])
+        mock_r = self.make_obs_cloud_data(200, con)
+        self.mock_obs_cloud_list_objects(return_value=mock_r)
+        self.mock_obs_cloud_get_objects(return_value=mock_r)
+        read_excel = pd.read_excel(Path(EXPECT_DATA_FILE, "mock_cve_data.xlsx"), sheet_name="cve_list")
+        self.mock_pandas_read_excel(return_value=read_excel)
+        self.mock_request(side_effect=[resp, resp, resp, resp, resp, mock_init_r])
+        mock_get_r = self.make_object_data(200)
+        self.mock_requests_get(side_effect=[mock_get_r])
+        self.assert_result()
+
+    def test_failed(self):
+        """
+        test failed
+        """
+        self.expect_str = """
+[ERROR] failed to start update.
+        """
+        resp = self.make_expect_data(200, 'startpart.txt')
+        self.command_params = ["--giteeid=Mary", "--token=release-tools", "I40321"]
+        con = self.read_file_content('mock_obs_data.json', folder=EXPECT_DATA_FILE)
+        for con_key in con['contents']:
+            con_key["key"] = "cve-manager-updateinfo/{}/{}".format(
+                datetime.date(datetime.date.today().year, datetime.date.today().month,
+                              datetime.date.today().day).strftime('%Y-%m-%d'), con_key["key"])
+        mock_r = self.make_obs_cloud_data(200, con)
+        self.mock_obs_cloud_list_objects(return_value=mock_r)
+        self.mock_obs_cloud_get_objects(return_value=mock_r)
+        read_excel = pd.read_excel(Path(EXPECT_DATA_FILE, "mock_cve_data.xlsx"), sheet_name="cve_list")
+        self.mock_pandas_read_excel(return_value=read_excel)
+        self.mock_request(side_effect=[resp, resp, resp, resp, resp, RequestException])
+        mock_get_r = self.make_object_data(200)
+        self.mock_requests_get(side_effect=[mock_get_r])
+        self.assert_result()
+
+    def test_validate_failed(self):
+        """
+        test validate failed
+        """
+        self.expect_str = """
+Parameter validation failed
+        """
+        self.command_params = ["--giteeid=Mary", "--token=release-tools", ""]
+        self.assert_result()
+
+    def test_request_status_408(self):
+        """test_request_status_408"""
+        self.expect_str = """
+[ERROR] no personnel authority
+"""
+        self.command_params = ["--giteeid=Mary", "--token=xxx", "I40321"]
+        mock_r = self.make_object_data(408)
+        self.mock_request(return_value=mock_r)
+        self.assert_result()
+
+    def test_request_raise_requestexception(self):
+        """test_request_raise_requestexception"""
+        self.command_params = ["--giteeid=Mary", "--token=xxx", "I40321"]
+        self.expect_str = """
+[ERROR] no personnel authority
+"""
+        self.mock_request(side_effect=[RequestException])
+        self.assert_result()
+
+    def test_no_permission(self):
+        """
+        test no permission
+        """
+        self.expect_str = """
+[ERROR] The current user does not have relevant operation permissions
+        """
+
+        self.command_params = ["--giteeid=onetwothree", "--token=xxx", "I40321"]
+        resp = self.make_expect_data(200, 'startpart.txt')
+        self.mock_request(side_effect=[resp])
+        self.assert_result()
+
+    def test_no_personnel_authority(self):
+        """
+        test no personnel authority
+        """
+        self.expect_str = """
+[ERROR] no personnel authority
+        """
+        self.command_params = ["--giteeid=Mary", "--token=xxx", "I40321"]
+        resp = self.make_expect_data(200, 'mock_incorrect_issue.txt')
+        self.mock_request(side_effect=[resp])
+        self.assert_result()
+
+    def test_resp_body_is_none(self):
+        """
+        test resp body is none
+        """
+        self.expect_str = """
+[ERROR] failed to start update.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=xxx", "I40321"]
+        resp = self.make_expect_data(200, 'startpart.txt')
+        issue_body_is_none_data = self.read_file_content('mock_issue_is_none.txt', folder=EXPECT_DATA_FILE,
+                                                         is_json=False)
+        mock_issue_body_is_none_r = self.make_object_data(200, issue_body_is_none_data)
+        self.mock_request(side_effect=[resp, mock_issue_body_is_none_r])
+        self.assert_result()
+
+    def test_already_operated(self):
+        """
+        test already operated
+        """
+        self.expect_str = """
+Issue has CVE content, have you already operated start update command
+[ERROR] failed to start update.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=xxx", "I40321"]
+        resp = self.make_expect_data(200, 'startpart.txt')
+        already_operated_data = self.read_file_content('already_operated.txt', folder=EXPECT_DATA_FILE,
+                                                       is_json=False)
+        mock_already_operated_r = self.make_object_data(200, already_operated_data)
+        self.mock_request(side_effect=[resp, mock_already_operated_r])
+        self.assert_result()
+
+    def test_download_file_failed(self):
+        """
+        test download file failed
+        """
+        self.expect_str = """
+[ERROR] failed to start update.
+        """
+        resp = self.make_expect_data(200, 'startpart.txt')
+        self.command_params = ["--giteeid=Mary", "--token=release-tools", "I40321"]
+        con = self.read_file_content('mock_obs_data.json', folder=EXPECT_DATA_FILE)
+        for con_key in con['contents']:
+            con_key["key"] = "cve-manager-updateinfo/{}/{}".format(
+                datetime.date(datetime.date.today().year, datetime.date.today().month,
+                              datetime.date.today().day).strftime('%Y-%m-%d'), con_key["key"])
+        mock_listobjects_r = self.make_obs_cloud_data(200, con)
+        self.mock_obs_cloud_list_objects(return_value=mock_listobjects_r)
+        mock_getobjects_r = self.make_object_data(400)
+        self.mock_obs_cloud_get_objects(return_value=mock_getobjects_r)
+        read_excel = pd.read_excel(Path(EXPECT_DATA_FILE, "mock_cve_data.xlsx"), sheet_name="cve_list")
+        self.mock_pandas_read_excel(return_value=read_excel)
+        self.mock_request(side_effect=[resp, resp, resp, resp, resp, resp, resp])
+        mock_get_r = self.make_object_data(200)
+        self.mock_requests_get(side_effect=[mock_get_r])
+        self.assert_result()
