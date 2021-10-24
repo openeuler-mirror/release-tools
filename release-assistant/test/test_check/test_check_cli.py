b0f6b56931282ed5aa58ee13a99b8710bbba4e72
jiangpengjuj <jiangpengju1@yeah.net>
2021-10-24 12:41:32 +0800

    自动创建issue时，支持在issue描述中附安装或自编译日志

--- release-assistant/test/test_check/test_check_cli.py
+++ release-assistant/test/test_check/test_check_cli.py
@@ -47,10 +47,11 @@ class TestCheck(TestMixin):
         mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
         mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
         mock_post_data = self.make_need_content('mock_post_data.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         self.mock_requests_post(return_value=mock_post_data)
         self.mock_request(
             side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
-                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, resp, resp])
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, mock_repo_list_data, resp])
         self.assert_result()
 
     def test_check_status_failed(self):
@@ -80,15 +81,16 @@ during the operation status, a failure occurred, and the cause of the error was 
         resp = self.make_expect_data(200, 'checkpart.txt')
         mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
         mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
+        mock_abnormal_r = self.make_need_content('mock_abnormal_issue.txt', MOCK_DATA_FILE)
         mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
         self.mock_request(
             side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
-                         resp, RequestException])
+                         resp, resp, RequestException, mock_abnormal_r])
         self.assert_result()
 
-    def test_send_repo_info_failed(self):
+    def test_send_repo_info_requests_post_failed(self):
         """
-        test send repo info failed
+        test send repo info requests post failed
         """
         self.expect_str = """
 [INFO] successfully update status in check part.
@@ -102,10 +104,102 @@ during the operation status, a failure occurred, and the cause of the error was 
         mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
         mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
         mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         self.mock_requests_post(return_value=None)
         self.mock_request(
             side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
-                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, resp, resp])
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, mock_repo_list_data, resp])
+        self.assert_result()
+
+    def test_send_repo_info_requestException(self):
+        """
+        test send repo info test_send_repo_info_requestException
+        """
+        self.expect_str = """
+[INFO] successfully update status in check part.
+[INFO] All issues are completed, the next step is sending repo to test platform.
+[ERROR] failed to send repo info.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=example", "--type=status", "--jenkinsuser=mary",
+                               "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
+        resp = self.make_expect_data(200, 'checkpart.txt')
+        self.mock_subprocess_check_output(return_value=b"published-everything-src")
+        mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
+        mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
+        mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
+        self.mock_requests_post(side_effect=[RequestException])
+        self.mock_request(
+            side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, mock_repo_list_data, resp])
+        self.assert_result()
+
+    def test_send_repo_info_error_code_400(self):
+        """
+        test send repo info error code 400
+        """
+        self.expect_str = """
+[INFO] successfully update status in check part.
+[INFO] All issues are completed, the next step is sending repo to test platform.
+[ERROR] failed to send repo info.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=example", "--type=status", "--jenkinsuser=mary",
+                               "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
+        resp = self.make_expect_data(200, 'checkpart.txt')
+        self.mock_subprocess_check_output(return_value=b"published-everything-src")
+        mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
+        mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
+        mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
+        mock_post_data = self.make_need_content('mock_error_post_data.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
+        self.mock_requests_post(return_value=mock_post_data)
+        self.mock_request(
+            side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, mock_repo_list_data, resp])
+        self.assert_result()
+
+    def test_send_repo_info_request_repo_url_failed(self):
+        """
+        test send repo info requests post failed
+        """
+        self.expect_str = """
+[INFO] successfully update status in check part.
+[INFO] All issues are completed, the next step is sending repo to test platform.
+[ERROR] failed to send repo info.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=example", "--type=status", "--jenkinsuser=mary",
+                               "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
+        resp = self.make_expect_data(200, 'checkpart.txt')
+        self.mock_subprocess_check_output(return_value=b"published-everything-src")
+        mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
+        mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
+        mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
+        self.mock_request(
+            side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, RequestException, resp])
+        self.assert_result()
+
+    def test_send_repo_info_get_update_list_failed(self):
+        """
+        test send repo info requests post failed
+        """
+        self.expect_str = """
+[INFO] successfully update status in check part.
+[INFO] All issues are completed, the next step is sending repo to test platform.
+[ERROR] failed to send repo info.
+        """
+        self.command_params = ["--giteeid=Mary", "--token=example", "--type=status", "--jenkinsuser=mary",
+                               "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
+        resp = self.make_expect_data(200, 'checkpart.txt')
+        self.mock_subprocess_check_output(return_value=b"published-everything-src")
+        mock_bugfix_r = self.make_need_content('mock_bugfix_issue.txt', MOCK_DATA_FILE)
+        mock_install_r = self.make_need_content('mock_install_issue.txt', MOCK_DATA_FILE)
+        mock_check_r = self.make_need_content('check_status_success.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
+        self.mock_request(
+            side_effect=[resp, resp, resp, resp, resp, mock_install_r, resp, resp, resp, mock_bugfix_r, mock_check_r,
+                         resp, resp, mock_install_r, mock_bugfix_r, resp, resp, resp, resp, mock_repo_list_data,
+                         RequestException])
         self.assert_result()
 
     def test_block_has_no_related_issues(self):
@@ -209,11 +303,11 @@ Parameter validation failed
         mock_add_repo_r = self.make_need_content('mock_add_repo_success.txt', MOCK_DATA_FILE)
         mock_exist_issues = self.make_need_content('exist_issues.txt', MOCK_DATA_FILE)
         mock_create_jenkins_comment = self.make_need_content('create_jenkins_comments_success.txt', MOCK_DATA_FILE)
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         mock_create_install_jenkins_comment = self.make_need_content('create_install_jenkins_comments_success.txt',
                                                                      MOCK_DATA_FILE)
         mock_create_build_jenkins_comment = self.make_need_content('create_build_jenkins_comments_success.txt',
                                                                    MOCK_DATA_FILE)
-        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         mock_create_build_issue = self.make_need_content('create_build_issue_success.txt', MOCK_DATA_FILE)
         mock_create_install_issue = self.make_need_content('create_install_issue_success.txt', MOCK_DATA_FILE)
         mock_checkpart_add_build = self.make_need_content('checkpart_add_build_success.txt', MOCK_DATA_FILE)
@@ -251,14 +345,13 @@ Parameter validation failed
                                "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
         self.prepare_jenkins_data()
         self.prepare_obs_data()
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         self.mock_subprocess_check_output(return_value=b"published-everything-src")
         mock_create_jenkins_comment = self.make_need_content('create_jenkins_comments_success.txt', MOCK_DATA_FILE)
         resp = self.make_expect_data(200, 'checkpart.txt')
-        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         self.mock_request(
             side_effect=[resp, resp, resp, resp, resp, resp, resp, resp, resp, resp, mock_repo_list_data,
-                         mock_create_jenkins_comment, resp, resp, resp, resp,
-                         RequestException])
+                         mock_create_jenkins_comment, resp, resp, resp, resp, RequestException])
         self.assert_result()
 
     def test_create_jenkins_comment_and_build_comment_and_install_comment_failed(self):
@@ -353,6 +446,35 @@ during the operation requires, a failure occurred, and the cause of the error wa
         self.mock_request(return_value=resp)
         self.assert_result()
 
+    def test_download_pkg_log_write_back_create_install_build_issue_failed(self):
+        """
+        test download pkg log write back create install build issue failed
+        """
+        self.expect_str = self.read_file_content("download_pkg_log_failed.txt",
+                                                 folder=EXPECT_DATA_FILE, is_json=False)
+        self.command_params = ["--giteeid=Mary", "--token=example", "--type=requires", "--jenkinsuser=mary",
+                               "--jenkinskey=marykey", "--ak=forexample", "--sk=forexample", "I40769"]
+        self.prepare_jenkins_data()
+        self.prepare_obs_data(get_objects_status_code=400)
+        resp = self.make_expect_data(200, 'checkpart.txt')
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
+        mock_add_repo_r = self.make_need_content('mock_add_repo_success.txt', MOCK_DATA_FILE)
+        mock_exist_issues = self.make_need_content('single_exist_issues.txt', MOCK_DATA_FILE)
+        mock_create_jenkins_comment = self.make_need_content('create_jenkins_comments_success.txt', MOCK_DATA_FILE)
+        mock_create_install_jenkins_comment = self.make_need_content('create_install_jenkins_comments_success.txt',
+                                                                     MOCK_DATA_FILE)
+        mock_create_build_jenkins_comment = self.make_need_content('create_build_jenkins_comments_success.txt',
+                                                                   MOCK_DATA_FILE)
+        mock_issue_comment = self.make_need_content('mock_issue_comment.txt', MOCK_DATA_FILE)
+        mock_create_build_issue = self.make_need_content('create_build_issue_success.txt', MOCK_DATA_FILE)
+        self.mock_request(
+            side_effect=[resp, resp, resp, resp, resp, resp, resp, resp, resp, resp, mock_repo_list_data,
+                         mock_create_jenkins_comment, resp, resp, resp, resp, mock_add_repo_r,
+                         mock_create_build_jenkins_comment, mock_create_install_jenkins_comment, resp, resp,
+                         mock_exist_issues, mock_issue_comment, mock_create_build_issue, RequestException,
+                         RequestException])
+        self.assert_result()
+
     def test_write_back_create_install_build_issue_failed(self):
         """
         write_back_create_install_build_issue_failed
@@ -364,9 +486,9 @@ during the operation requires, a failure occurred, and the cause of the error wa
         self.prepare_jenkins_data()
         self.prepare_obs_data()
         resp = self.make_expect_data(200, 'checkpart.txt')
+        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         mock_add_repo_r = self.make_need_content('mock_add_repo_success.txt', MOCK_DATA_FILE)
         mock_exist_issues = self.make_need_content('exist_issues.txt', MOCK_DATA_FILE)
-        mock_repo_list_data = self.make_need_content('repo_list_data.txt', MOCK_DATA_FILE)
         mock_create_jenkins_comment = self.make_need_content('create_jenkins_comments_success.txt', MOCK_DATA_FILE)
         mock_create_install_jenkins_comment = self.make_need_content('create_install_jenkins_comments_success.txt',
                                                                      MOCK_DATA_FILE)
@@ -375,7 +497,7 @@ during the operation requires, a failure occurred, and the cause of the error wa
         self.mock_request(
             side_effect=[resp, resp, resp, resp, resp, resp, resp, resp, resp, resp, mock_repo_list_data,
                          mock_create_jenkins_comment, resp, resp, resp, resp, mock_add_repo_r,
-                         mock_create_build_jenkins_comment, mock_create_install_jenkins_comment, resp,resp,
+                         mock_create_build_jenkins_comment, mock_create_install_jenkins_comment, resp, resp,
                          mock_exist_issues, RequestException, RequestException])
         self.assert_result()
 
@@ -438,11 +560,14 @@ during the operation requires, a failure occurred, and the cause of the error wa
         self.mock_jenkins_get_build_console_output(
             return_value=self.read_file_content('get_build_console_output.txt', folder=MOCK_DATA_FILE, is_json=False))
 
-    def prepare_obs_data(self, file_name='mock_obs_data.json', list_status_code=200, delete_status_code=200):
+    def prepare_obs_data(self, file_name='mock_obs_data.json', list_status_code=200, delete_status_code=200,
+                         get_objects_status_code=200):
         """
         prepare obs mock data
         """
         mock_obs_list_objects_r = self.make_need_obs_cloud_data(file_name, MOCK_DATA_FILE, list_status_code)
         mock_obs_delete_objects_r = self.make_need_obs_cloud_data(file_name, MOCK_DATA_FILE, delete_status_code)
+        mock_getobjects_r = self.make_object_data(get_objects_status_code)
+        self.mock_obs_cloud_get_objects(return_value=mock_getobjects_r)
         self.mock_obs_cloud_list_objects(return_value=mock_obs_list_objects_r)
         self.mock_obs_cloud_delete_object(return_value=mock_obs_delete_objects_r)
